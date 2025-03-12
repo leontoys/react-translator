67f4c51b6e2f3aec33702d9ff3ab0398d1b06701
leontoys <svliyon@gmail.com>
2025-03-13 09:58:31 +1100

    fixed-language-selector

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -1,26 +1,24 @@
-//list of languages
 const LANGUAGES = {
     "Acehnese (Arabic script)": "ace_Arab",
     "Acehnese (Latin script)": "ace_Latn",
     "Afrikaans": "afr_Latn",
-    //others
+    //...
     "English" : "eng_Latn",
     "French" : "fra_Latn",
-    "Zulu": "zul_Latn"
-}
-
-//returns a select control
-const LanguageSelector = ({type,onChange,defaultLanguage})=>{
-    return(
-        <div className="language-selector">
-            <label>{type}: </label>
-            <select onChange={onChange} defaultValue={defaultLanguage}>
-                {Object.entries(LANGUAGES).map(([key,value])=>{
-                    return <option key={key} value={value}></option>
-                })}
-            </select>
-        </div>
+    "Zulu": "zul_Latn",
+  }
+  
+  const LanguageSelector = ({ type, onChange, defaultLanguage }) => {
+    return (
+      <div className='language-selector'>
+        <label>{type}: </label>
+        <select onChange={onChange} defaultValue={defaultLanguage}>
+          {Object.entries(LANGUAGES).map(([key, value]) => {
+            return <option key={key} value={value}>{key}</option>
+          })}
+        </select>
+      </div>
     )
-}
+  }
 
-export default LanguageSelector
+  export default LanguageSelector
