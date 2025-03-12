1b34a25bca7428e764232f950dbe9ead4ac7a7e7
leontoys <svliyon@gmail.com>
2025-03-13 09:07:59 +1100

    restarting-march-13

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,29 @@ import { useState, useRef, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  //for worker object - create reference
+  const worker = useRef(null)
+
+  //as soon as App is mounted, get worker
+  useEffect(()=>{
+
+    //create worker if doesn't exist
+    worker.current ??= new Worker( new URL('./worker.js', import.meta.url ), {
+      type : 'module'
+    } )
+
+    //call back for message received
+    const onMessageReceived = (e)=>{
+      //TODO
+    }
+
+    //link call back to event
+    worker.current.addEventListener('message',onMessageReceived)
+
+    //clean up when component is unmounted
+    return ()=>worker.current.removeEventListener('message',onMessageReceived)
+
+  }) 
 
   return (
       <div className="card">
