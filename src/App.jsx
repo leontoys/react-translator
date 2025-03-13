532224d5bb9ae7a6ba23d09807f9e202372ed296
leontoys <svliyon@gmail.com>
2025-03-13 11:25:06 +1100

    react-translator-done-ready-to-deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 //added more hooks
-import { useState, useRef, useEffect } from 'react' 
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 import LanguageSelector from './components/LanguageSelector'
 import Progress from './components/Progress'
@@ -8,40 +8,42 @@ function App() {
   //for worker object - create reference
   const worker = useRef(null)
   //model loading - progress indicators
-  const [ready,setReady] = useState(null)
-  const [disabled,setDisabled] = useState(false)
-  const [progressItems,setProgressItems] = useState([])
+  const [ready, setReady] = useState(null)
+  const [disabled, setDisabled] = useState(false)
+  const [progressItems, setProgressItems] = useState([])
 
   //translation default languages and inputs
-  const [input,setInput] = useState('This is a demo')
-  const [sourceLanguage,setSourceLanguage] = useState('eng_Latn')
-  const [targetLanguage,setTargetLanguage] = useState('fra_Latn')
-  const [output,setOutput] = useState('')
+  const [input, setInput] = useState('This is a demo')
+  const [sourceLanguage, setSourceLanguage] = useState('eng_Latn')
+  const [targetLanguage, setTargetLanguage] = useState('fra_Latn')
+  const [output, setOutput] = useState('')
+
+  //console.log("progress-items",progressItems)
 
   //as soon as App is mounted, get worker
-  useEffect(()=>{
+  useEffect(() => {
 
     //create worker if doesn't exist
-    worker.current ??= new Worker( new URL('./worker.js', import.meta.url ), {
-      type : 'module'
-    } )
+    worker.current ??= new Worker(new URL('./worker.js', import.meta.url), {
+      type: 'module'
+    })
 
     //call back for message received
-    const onMessageReceived = (e)=>{
-      console.log("event",e.data)
+    const onMessageReceived = (e) => {
+      console.log("event", e.data)
       //TODO
       switch (e.data.status) {//based on the status update from worker
         case 'initiate':
-        //model file starts loading
+          //model file starts loading
           setReady(false)
-          setProgressItems(prev => [...prev,e.data])//keep appending the files info
+          setProgressItems(prev => [...prev, e.data])//keep appending the files info
           break;
 
         //update progress percentages
         case 'progress':
-          setProgressItems(prev=>prev.map(item=>{
-            if(item.file === e.data.file){
-              return {...item, progress : e.data.progress}//update progress
+          setProgressItems(prev => prev.map(item => {
+            if (item.file === e.data.file) {
+              return { ...item, progress: e.data.progress }//update progress
             }
             return item
           }))
@@ -51,21 +53,21 @@ function App() {
         //once all files are loaded, remove the pgoress indicators
         case 'done':
           setProgressItems(
-            prev=>prev.filter(item=> item.file!== e.data.file))
+            prev => prev.filter(item => item.file !== e.data.file))
           break;
 
         //pipeline is ready
         case 'ready':
-          setReady(true)  
+          setReady(true)
           break;
-          
+
         //case update
         case 'update':
           //from the streamer, keep updating output
-          setOutput( output + e.data.output )
+          setOutput(output + e.data.output)
           break;
-          
-            
+
+
         case 'complete':
           //enable button
           setDisabled(false)
@@ -74,66 +76,66 @@ function App() {
     }
 
     //link call back to event
-    worker.current.addEventListener('message',onMessageReceived)
+    worker.current.addEventListener('message', onMessageReceived)
 
     //clean up when component is unmounted
-    return ()=>worker.current.removeEventListener('message',onMessageReceived)
+    return () => worker.current.removeEventListener('message', onMessageReceived)
 
-  }) 
+  })
 
   //call worker to translate
-  const translate = ()=>{
+  const translate = () => {
     //disable button to avoid multiple clicks
     setDisabled(true)
     //clear output
     setOutput("")
     //call worker
     worker.current.postMessage({
-      text : input,
-      sourceLanguage : sourceLanguage,
-      targetLanguage : targetLanguage
+      text: input,
+      sourceLanguage: sourceLanguage,
+      targetLanguage: targetLanguage
     })
   }
 
   return (
-      <div className="card">
+    <div className="card">
 
-        <h1>Translate Text</h1>
+      <h1>Translate Text</h1>
 
-        <div className='container'>
+      <div className='container'>
 
-          <div className='language-container'>
-            <LanguageSelector type={"Source"} defaultLanguage={"eng_Latn"} 
-            onChange={e=>setSourceLanguage(e.target.value)}
-            ></LanguageSelector>
-            <LanguageSelector type={"Target"} defaultLanguage={"fra_Latn"}
-            ></LanguageSelector>
-          </div>
-
-          <div className='textbox-container'>
-            <textarea value={input}   rows={3}  onChange={e=>setInput(e.target.value)}></textarea>
-            <textarea value={output}  rows={3}  readOnly></textarea>
-          </div>
-
+        <div className='language-container'>
+          <LanguageSelector type={"Source"} defaultLanguage={"eng_Latn"}
+            onChange={e => setSourceLanguage(e.target.value)}
+          ></LanguageSelector>
+          <LanguageSelector type={"Target"} defaultLanguage={"fra_Latn"}
+          ></LanguageSelector>
         </div>
 
-        <button disabled={disabled} onClick={translate}>Translate</button>
-
-        {/* Progress Indicators - for model loading  */}
-        <div className='progress-bars-container'>
-          {ready === false && (
-            <label>Loading models...(run only once)</label>
-          )}
-          {progressItems.map(data=>{
-            <div key={data.file}>
-              <Progress text={data.file} percentage={data.progress}></Progress>
-            </div>
-          })}
+        <div className='textbox-container'>
+          <textarea value={input} rows={3} onChange={e => setInput(e.target.value)}></textarea>
+          <textarea value={output} rows={3} readOnly></textarea>
         </div>
-
-        <p>powered by Transformers.js</p>
 
       </div>
+
+      <button disabled={disabled} onClick={translate}>Translate</button>
+
+      {/* Progress Indicators - for model loading  */}
+      <div className='progress-bars-container'>
+        {ready === false && (
+          <label>Loading models... (only run once)</label>
+        )}
+        {progressItems.map(data => (
+          <div key={data.file}>
+            <Progress text={data.file} percentage={data.progress} />
+          </div>
+        ))}
+      </div>
+
+      <p>powered by Transformers.js</p>
+
+    </div>
   )
 }
 
